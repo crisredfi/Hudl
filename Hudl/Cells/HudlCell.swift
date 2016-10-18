b805be9555eaa0cc94d7d07465c82054257d845a
Aleix Guri <aleix.guri@xing.com>
2016-10-18 19:22:38 +0200

    Linked cell with class

---
+++ Hudl/Cells/HudlCell.swift
@@ -0,0 +1,21 @@
+//
+//  HudlCell.swift
+//  Hudl
+//
+//  Created by Aleix Guri on 18/10/2016.
+//  Copyright © 2016 Aleix Guri. All rights reserved.
+//
+
+import UIKit
+
+class HudlCell: UICollectionViewCell {
+
+    @IBOutlet weak var youtubeImage: UIImageView!
+
+    @IBOutlet weak var youtubeTitle: UILabel!
+
+    @IBOutlet weak var youtubeSubtitle: UILabel!
+
+    @IBOutlet weak var youtubeFavourite: UIButton!
+
+}
