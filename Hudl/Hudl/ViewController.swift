0a970bd4a2bb5f83fdae0baefc1019e24f049f9a
Aleix Guri <aleix.guri@xing.com>
2016-10-18 17:35:53 +0200

    First commit

---
+++ Hudl/Hudl/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hudl
+//
+//  Created by Aleix Guri on 18/10/2016.
+//  Copyright © 2016 Aleix Guri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
