3ac9b739adb790ba35de6cb205a2a5dc7721382e
Aleix Guri <aleix.guri@xing.com>
2016-10-18 18:50:46 +0200

    updated layout to manually handle better iPad and horizontal states. Added Custom cell class

--- Hudl/Hudl/ViewController.swift
+++ Hudl/Hudl/ViewController.swift
@@ -8,7 +8,9 @@
 
 import UIKit
 
-class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+
+    fileprivate let reuseIdentifier = "hudlCell"
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,10 +32,32 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
     // The cell that is returned must be retrieved from a call to -dequeueReusableCellWithReuseIdentifier:forIndexPath:
     public func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
 
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier,
+                                                      for: indexPath)
+        cell.backgroundColor = UIColor.black
+        // Configure the cell
+        return cell
     }
     
+    // MARK: UICollectionViewDelegateFlowLayout
+    // lets modify the width of the collection view for iphones to fit full screen
+    public func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
 
-    
+        var width = UIScreen.main.bounds.width
+        let cell = collectionView.cellForItem(at: indexPath)
+        // check for heigh. if unwrap fails, assign default height.
+        let height = cell?.bounds.size.height ?? CGFloat(185)
+
+        switch UIDevice.current.userInterfaceIdiom {
+        case .pad, .unspecified:
+            width = width / 2
+            break
+        default:
+            break
+        }
+
+        return CGSize(width: width, height: height)
+    }
 
 
 }
